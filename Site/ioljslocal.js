179a5d5bfe4381d953a4a74fa839f8088d32962b
purplexmoss <purplexmoss@gmail.com>
2020-09-16 19:53:40 -0600

    Initial commit -Starting files: Added necessary templates pages, CSS and images.

---
+++ Site/ioljslocal.js
@@ -0,0 +1,21 @@
+//project cards
+const cards = document.querySelectorAll(".card");
+
+function transition() {
+  if (this.classList.contains("active")) {
+    this.classList.remove("active");
+  } else {
+    this.classList.add("active");
+  }
+}
+
+cards.forEach(card => card.addEventListener("click", transition));
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
